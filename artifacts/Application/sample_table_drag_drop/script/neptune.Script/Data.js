f740c137a04288266c762009322bff9ce8066464
Emmanuella Ndukwe <emmanuella.ndukwe@neptune-software.com>
2024-07-08 13:51:06 +0000

    Removed broken apps

---
+++ artifacts/Application/sample_table_drag_drop/script/neptune.Script/Data.js
@@ -0,0 +1,104 @@
+const tableData = [
+  {
+    "people": 15423,
+    "city": "New York",
+    "houses": 3872
+  },
+  {
+    "people": 2743,
+    "city": "Los Angeles",
+    "houses": 683
+  },
+  {
+    "people": 9345,
+    "city": "Chicago",
+    "houses": 2390
+  },
+  {
+    "people": 4782,
+    "city": "Houston",
+    "houses": 1197
+  },
+  {
+    "people": 6321,
+    "city": "Phoenix",
+    "houses": 1580
+  },
+  {
+    "people": 2897,
+    "city": "Philadelphia",
+    "houses": 725
+  },
+  {
+    "people": 5228,
+    "city": "San Antonio",
+    "houses": 1307
+  },
+  {
+    "people": 3725,
+    "city": "San Diego",
+    "houses": 932
+  },
+  {
+    "people": 8459,
+    "city": "Dallas",
+    "houses": 2142
+  },
+  {
+    "people": 4671,
+    "city": "San Jose",
+    "houses": 1181
+  },
+  {
+    "people": 3157,
+    "city": "Austin",
+    "houses": 799
+  },
+  {
+    "people": 5874,
+    "city": "Jacksonville",
+    "houses": 1485
+  },
+  {
+    "people": 4912,
+    "city": "Fort Worth",
+    "houses": 1238
+  },
+  {
+    "people": 2789,
+    "city": "Columbus",
+    "houses": 704
+  },
+  {
+    "people": 6348,
+    "city": "Charlotte",
+    "houses": 1603
+  },
+  {
+    "people": 5496,
+    "city": "San Francisco",
+    "houses": 1387
+  },
+  {
+    "people": 7265,
+    "city": "Indianapolis",
+    "houses": 1831
+  },
+  {
+    "people": 5139,
+    "city": "Seattle",
+    "houses": 1297
+  },
+  {
+    "people": 3952,
+    "city": "Denver",
+    "houses": 996
+  },
+  {
+    "people": 4683,
+    "city": "Washington",
+    "houses": 1179
+  }
+]
+
+modeloTable.setData(tableData);
